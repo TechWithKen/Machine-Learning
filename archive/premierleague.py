a306af9b9cd71f42e29984cd337e3c3aa3448f82
TechWithKen <ti664469@gmail.com>
2026-01-11 02:35:14 +0100

    added another file to start working on

---
+++ archive/premierleague.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+premier_league_result = pd.read_csv("results.csv")
+premier_league_stat = pd.read_csv("stats.csv")
+
+
+
+
+#print(premier_league_result.head(50))
+print(premier_league_stat.head(50))

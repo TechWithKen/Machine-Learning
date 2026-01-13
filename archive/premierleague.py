58afe1569710b9d666dc8036a26301acea4e89e6
TechWithKen <ti664469@gmail.com>
2026-01-13 11:03:10 +0100

    Cleaned up the code more for reusability

--- archive/premierleague.py
+++ archive/premierleague.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 def create_outcome(team_side, result_map, dataframe):
@@ -11,8 +10,11 @@ def create_outcome(team_side, result_map, dataframe):
     return team
 
 def divide_home_away_team_results():
-    home = create_outcome("home_team", {"D": "Draw", "H": "Win", "A": "Loss"}, dataframe=premier_league_result)
-    away = create_outcome("away_team", {"D": "Draw", "H": "Loss", "A": "Win"}, dataframe=premier_league_result)
+
+    # for home team H = Win, A = Loss
+    # for the away team, H = Loss, A = Win
+    home = create_outcome("home_team", {"D": "D", "H": "W", "A": "L"}, dataframe=premier_league_result)
+    away = create_outcome("away_team", {"D": "D", "H": "L", "A": "W"}, dataframe=premier_league_result)
 
 
     all_results = pd.concat([home, away], ignore_index=True)
@@ -41,7 +43,7 @@ def each_team_total_goals():
 
 def count_goals_conceded(side_1, side_2, dataframe):
     
-    team = dataframe[[f"{side_1}_team", f"{side_2}_goals"]]
+    team = dataframe[[f"{side_1}_team", f"{side_2}_goals"]] # for home team select away goal, for away team select home goals.
     team['club'] = team[f"{side_1}_team"]
     team["goals"] = team[f"{side_2}_goals"]
     team = team[["club", "goals"]]
@@ -65,16 +67,17 @@ def get_premier_league_table(home_away_team, each_team_goals, goal_conceded):
     home_away_team_results = home_away_team.pivot_table(columns="outcome", index="club", aggfunc="size")
     played = home_away_team_results.sum(axis=1)
     home_away_team_results.insert(0, "P", played)
-    home_away_team_results["PTS"] = (home_away_team_results["Draw"]*1) + (home_away_team_results["Win"]*3)
+    home_away_team_results["PTS"] = (home_away_team_results["D"]*1) + (home_away_team_results["W"]*3)  # Win = +3 pts, Draw = +1 pts
 
-    league_table = home_away_team_results.merge(each_team_goals, on="club", how="inner")
+    league_table = home_away_team_results.merge(each_team_goals, on="club", how="inner").merge(goal_conceded, on="club", how="inner")
 
-    league_table = league_table.merge(goal_conceded, on="club", how="inner")
-    league_table.rename(columns={"goals_x": "GF", "Draw": "D", "Loss": "L", "Win": "W", "goals_y": "GA"}, inplace=True)
+
+    league_table.rename(columns={"goals_x": "GF", "goals_y": "GA"}, inplace=True) # goals_x = Goals Score, and goals_y = Goals Conceded
     league_table["GD"] = league_table["GF"] - league_table["GA"]
     league_table = league_table.sort_values(by="PTS", ascending=False)
 
 
+    # Re-arranging the dataframe so it appears like a conventional league table.
     remove_col = league_table.columns.tolist()
     remove_col.remove("PTS")
     remove_col.append("PTS")
@@ -88,14 +91,16 @@ premier_league_results = pd.read_csv("results.csv")
 premier_league_stat = pd.read_csv("stats.csv")
 
 
-years = []
-win_team = []
-winners = {"Year": years, "Winner": win_team}
+# You can edit the code from here to do other things, The dataframe itself has been well formated to get the league table.
+
+# Example Task - Analyze the goal difference of multiple teams across different seasons.
+
 
 goal_difference_team1 = []
 goal_difference_team2 = []
+
 seasons = premier_league_results["season"].unique().tolist()
-team1 = "Manchester United"
+team1 = "Manchester City"
 team2 = "Chelsea"
 
 for season in seasons:
@@ -106,27 +111,19 @@ for season in seasons:
                     goal_conceded()))
     print(season)
     print(winning_team)
-    goal_difference_team1.append(winning_team.loc[team1, "GD"].astype("int64"))
-    goal_difference_team2.append(winning_team.loc[team2, "GD"].astype("int64"))
-#     years.append(season), win_team.append(winning_team.index[0])
-
-# winning_dataframe = pd.DataFrame(winners)
-
-# print(winning_dataframe)
-
- 
-# print(goal_difference)
+    goal_difference_team1.append(winning_team.loc[team1, "GF"].astype("int64"))
+    goal_difference_team2.append(winning_team.loc[team2, "GF"].astype("int64"))
 
 
-# Plotting the graph
+# Visualizing a line graph for easy understanding of the analysis
 
 plt.figure(figsize=(12,6))
 plt.plot(seasons, goal_difference_team1, marker='o', linestyle='-', color='red', label=team1)
 plt.plot(seasons, goal_difference_team2, marker='s', linestyle='--', color='blue', label=team2)
 
-plt.title("Goal Difference per Season")
+plt.title("Goal Scored per Season")
 plt.xlabel("Season")
-plt.ylabel("Goal Difference (GD)")
+plt.ylabel("Goal Difference (Goal Scored)")
 plt.xticks(rotation=45)  # rotate season labels
 plt.grid(True)
 plt.legend()

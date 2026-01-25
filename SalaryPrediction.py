67ad6ff28ef9c1a827daae5a56daad1e74f5436e
TechWithKen <ti664469@gmail.com>
2026-01-25 02:02:34 +0100

    deleted

--- SalaryPrediction.py
+++ SalaryPrediction.py
@@ -13,7 +13,7 @@ def get_salary():
 
     salary_dataset = pd.read_csv("./ds_salaries.csv")
     salary_dataset = salary_dataset.drop_duplicates()
-    salary = np.log1p(salary_dataset["salary_in_usd"]) #Convert the salary to logarithm, so the difference between a large and medium price won't be large.
+    salary = (salary_dataset["salary_in_usd"]) #Convert the salary to logarithm, so the difference between a large and medium price won't be large.
     salary_dataset = salary_dataset[['experience_level','job_title', 'company_location', "employment_type", "remote_ratio", "company_size"]]
     
     return {"salary_dataset": salary_dataset, "salary": salary}
@@ -26,18 +26,16 @@ def preprocess_data():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     categorical_feature = ["experience_level", "employment_type"]
-    categorical_encoding = OneHotEncoder(drop="first", sparse_output=False)
-
     ordinal_feature = [["S", "M", "L"]]
     column = ["company_size"]
-    ordinal_encoding = OrdinalEncoder(categories=ordinal_feature)
-
     target_cols = ["job_title", "company_location"]
-    target = TargetEncoder(cols=target_cols)
 
-    preprocess = ColumnTransformer(transformers=[("onehot", categorical_encoding, categorical_feature),
-                                                ('ord', ordinal_encoding, column),
-                                                ("tar", target, target_cols)], remainder="passthrough")
+
+    preprocess = ColumnTransformer(transformers=[
+                                                ("onehot", Pipeline(steps=[("encoder", OneHotEncoder(drop="first", sparse_output=False))]), categorical_feature),
+                                                ('ord', Pipeline(steps=[("enconde_order", OrdinalEncoder(categories=ordinal_feature))]), column),
+                                                ("tar", Pipeline(steps=[("encode_target", TargetEncoder())]), target_cols)
+                                                ], remainder="passthrough")
 
 
     return {"X_train": X_train, "X_test": X_test, "y_train": y_train, "y_test": y_test, "preprocess": preprocess}
